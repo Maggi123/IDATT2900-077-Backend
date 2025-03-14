d7e26b0a56145db1de9cac374fe319656adc8f1b
Magnus Gjerlaugsen <maggj@stud.ntnu.no>
2025-03-14 11:35:17 +0100

    Update ESlint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,14 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.node } },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    ignores: ["von-network/*"],
+  },
 ];
