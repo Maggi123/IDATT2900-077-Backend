52adc1a2478a4047dba5bf6406df59e9a5c4739d
Magnus Gjerlaugsen <magnus.gjerlaugsen@oicloud.com>
2025-04-01 16:50:42 +0200

    Use agent logger in agent.js

--- src/agent.js
+++ src/agent.js
@@ -74,8 +74,8 @@ export async function createIssuer(agent, issuerId) {
     issuerRecord =
       await agent.modules.openid4VcIssuer.getIssuerByIssuerId(issuerId);
   } catch (e) {
-    console.error(e);
-    console.log("No issuer record stored, creating new issuer.");
+    agent.config.logger.error(e);
+    agent.config.logger.info("No issuer record stored, creating new issuer.");
   }
 
   if (issuerRecord) {
@@ -84,7 +84,7 @@ export async function createIssuer(agent, issuerId) {
       display: display,
       credentialConfigurationsSupported: supportedCredentials,
     });
-    console.log("Updated issuer metadata");
+    agent.config.logger.info("Updated issuer metadata");
     return;
   }
 
@@ -106,7 +106,7 @@ export async function setDid(agent) {
   if ((await agent.dids.getCreatedDids({ method: "indy" })) < 1) {
     let endorserNym;
 
-    console.log(
+    agent.config.logger.info(
       `Initializing backend for the first time.
     Please open the ledgers web interface here: http://${process.env.BACKEND_INDY_NETWORK_IP}:9000/browse/domain`,
     );
@@ -133,7 +133,7 @@ export async function setDid(agent) {
     const didLastColonIndex = backendDid.didState.did.lastIndexOf(":");
     const didShort = backendDid.didState.did.substring(didLastColonIndex + 1);
 
-    console.log(
+    agent.config.logger.info(
       `Please open the ledgers web interface here: http://${process.env.BACKEND_INDY_NETWORK_IP}:9000
     To add a DID to the ledger, choose the "Register from DID" radio button under the "Authenticate a New DID" section.
     Fill out the mandatory fields with the values given below.\n
@@ -156,7 +156,7 @@ export async function setDid(agent) {
     });
 
     if (backendDid.didState.state === "failed") {
-      console.error(
+      agent.config.logger.error(
         `Unable to create an endorser DID for backend. Cause: ${backendDid.didState.reason}`,
       );
       process.exit(1);
@@ -164,22 +164,21 @@ export async function setDid(agent) {
 
     did = backendDid.didState.did;
     fs.writeFile("did.txt", did, (err) => {
-      if (err) console.error(err);
+      if (err) agent.config.logger.error(err);
     });
   } else {
     try {
       did = fs.readFileSync("did.txt").toString();
     } catch (err) {
-      console.error(
-        "Unable to open file containing backends DID. Cause: ",
-        err,
+      agent.config.logger.error(
+        `Unable to open file containing backends DID. Cause: ${err}`,
       );
-      console.error(
+      agent.config.logger.error(
         "Please delete backends wallet. It can normally be found under ~/.afj/data/wallet/backend",
       );
       process.exit(1);
     }
-    console.log(did);
+    agent.config.logger.info(did);
   }
 
   return did;
@@ -298,7 +297,7 @@ export async function createPrescriptionOffer(agent, issuerId, prescriptionId) {
     OpenId4VcIssuerEvents.IssuanceSessionStateChanged,
     function handler(event) {
       if (event.payload.issuanceSession.id === issuanceSession.id) {
-        console.log(
+        agent.config.logger.info(
           "Issuance session state changed to ",
           event.payload.issuanceSession.state,
         );
