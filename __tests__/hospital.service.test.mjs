a24054c5a2be2a520002db8e8228ffad0e656861
Magnus Gjerlaugsen <magnus.gjerlaugsen@oicloud.com>
2025-04-07 12:43:15 +0200

    Add a test for createPrescriptionOffer in hospital service

--- __tests__/hospital.service.test.mjs
+++ __tests__/hospital.service.test.mjs
@@ -1,5 +1,17 @@
 import { sampleMedicationRequest } from "./__data__/smartSampleData.mjs";
-import { getPrescriptionClaims } from "../src/service/hospital.service.mjs";
+import {
+  getPrescriptionClaims,
+  createPrescriptionOffer,
+} from "../src/service/hospital.service.mjs";
+import { describe } from "vitest";
+import { MyLogger } from "../src/util/logger.mjs";
+import { EventEmitter, LogLevel } from "@credo-ts/core";
+import {
+  OpenId4VcIssuanceSessionState,
+  OpenId4VcIssuerApi,
+} from "@credo-ts/openid4vc";
+import { OpenId4VcIssuanceSessionRecord } from "@credo-ts/openid4vc/build/openid4vc-issuer/repository";
+import { agentDependencies } from "@credo-ts/node";
 
 describe("hospital tests", () => {
   afterEach(() => {
@@ -31,6 +43,65 @@ describe("hospital tests", () => {
 
     it("should throw when id is not valid", async () => {
       await expect(getPrescriptionClaims(1)).rejects.toThrow();
+    });
+  });
+
+  describe("createPrescriptionOffer", () => {
+    const verySimpleAgentMock = {
+      config: {
+        logger: new MyLogger(LogLevel.off),
+      },
+      events: new EventEmitter(agentDependencies, undefined),
+      modules: {
+        openid4VcIssuer: new OpenId4VcIssuerApi(
+          undefined,
+          undefined,
+          undefined,
+        ),
+      },
+    };
+
+    it("should create prescription offer", async () => {
+      const createCredentialOfferMock = vi.spyOn(
+        verySimpleAgentMock.modules.openid4VcIssuer,
+        "createCredentialOffer",
+      );
+
+      const eventOnMock = vi.spyOn(verySimpleAgentMock.events, "on");
+
+      const mockedCredentialOfferReturn = {
+        issuanceSession: new OpenId4VcIssuanceSessionRecord({
+          state: OpenId4VcIssuanceSessionState.OfferCreated,
+          issuer: "issuer",
+          credentialOfferId: "credentialOfferId",
+          credentialOfferPayload: undefined,
+        }),
+        credentialOffer: "credential-offer",
+      };
+
+      createCredentialOfferMock.mockResolvedValue(mockedCredentialOfferReturn);
+
+      const result = await createPrescriptionOffer(
+        verySimpleAgentMock,
+        "test",
+        1,
+        1,
+      );
+
+      expect(result).toBe(mockedCredentialOfferReturn.credentialOffer);
+      expect(createCredentialOfferMock).toHaveBeenCalledTimes(1);
+      expect(createCredentialOfferMock).toHaveBeenCalledWith({
+        issuerId: "test",
+        offeredCredentials: ["Prescription"],
+        preAuthorizedCodeFlowConfig: {
+          userPinRequired: false,
+        },
+        issuanceMetadata: {
+          prescriptionId: 1,
+          validityDays: 1,
+        },
+      });
+      expect(eventOnMock).toHaveBeenCalledTimes(1);
     });
   });
 });
