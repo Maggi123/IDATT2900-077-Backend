a42bc22ad75e5d7bb5ba3a5bf0c3710f23809492
Magnus Gjerlaugsen <magnus.gjerlaugsen@oicloud.com>
2025-04-08 17:13:42 +0200

    Add some more tests for hospital issuer controller

--- __tests__/controller/hospital.issuer.controller.test.mjs
+++ __tests__/controller/hospital.issuer.controller.test.mjs
@@ -1,11 +1,15 @@
 import express from "express";
 import request from "supertest";
+import { LogLevel } from "@credo-ts/core";
 
 import {
   HOSPITAL_ISSUER_PRESCRIPTIONS_PATH,
   HOSPITAL_ISSUER_ROUTER_PATH,
   setupHospitalIssuerRouter,
 } from "#src/controller/hospital.issuer.controller.mjs";
+import { getMedicationRequest } from "#src/service/smart.service.mjs";
+import { createPrescriptionOffer } from "#src/service/hospital.issuer.service.mjs";
+import { MyLogger } from "#src/util/logger.mjs";
 
 describe("hospital issuer controller tests", () => {
   let app;
@@ -27,15 +31,27 @@ describe("hospital issuer controller tests", () => {
           },
         ],
       }),
+      getMedicationRequest: vi.fn().mockImplementation((id) => {
+        if (id === sampleMedicationRequest.id) return sampleMedicationRequest;
+        throw new Error("Invalid ID");
+      }),
     };
   });
-  vi.mock("#src/service/hospital.issuer.service.mjs");
+  vi.mock("#src/service/hospital.issuer.service.mjs", () => ({
+    createPrescriptionOffer: vi.fn().mockResolvedValue("offer"),
+  }));
+
+  const simpleAgentMock = {
+    config: {
+      logger: new MyLogger(LogLevel.off),
+    },
+  };
 
   beforeEach(() => {
     app = express();
     app.use(
       HOSPITAL_ISSUER_ROUTER_PATH,
-      setupHospitalIssuerRouter(undefined, "issuer"),
+      setupHospitalIssuerRouter(simpleAgentMock, "issuer"),
     );
     app.use(express.static("public"));
     app.set("view engine", "pug");
@@ -59,5 +75,31 @@ describe("hospital issuer controller tests", () => {
       HOSPITAL_ISSUER_ROUTER_PATH + HOSPITAL_ISSUER_PRESCRIPTIONS_PATH,
     );
     expect(response.status).toBe(200);
+  });
+
+  it(`should send 200 when getting ${HOSPITAL_ISSUER_ROUTER_PATH + HOSPITAL_ISSUER_PRESCRIPTIONS_PATH}/:id/offer with valid id and query parameters`, async () => {
+    const response = await request(app).get(
+      `${HOSPITAL_ISSUER_ROUTER_PATH + HOSPITAL_ISSUER_PRESCRIPTIONS_PATH}/100/offer?validity=1`,
+    );
+    expect(response.status).toBe(200);
+    expect(getMedicationRequest).toHaveBeenCalledTimes(1);
+    expect(getMedicationRequest).toHaveBeenCalledWith("100");
+    expect(createPrescriptionOffer).toHaveBeenCalledTimes(1);
+    expect(createPrescriptionOffer).toHaveBeenCalledWith(
+      simpleAgentMock,
+      "issuer",
+      "100",
+      1,
+    );
+  });
+
+  it(`should send 404 when getting ${HOSPITAL_ISSUER_ROUTER_PATH + HOSPITAL_ISSUER_PRESCRIPTIONS_PATH} with invalid id`, async () => {
+    const response = await request(app).get(
+      `${HOSPITAL_ISSUER_ROUTER_PATH + HOSPITAL_ISSUER_PRESCRIPTIONS_PATH}/1/offer?validity=1`,
+    );
+    expect(response.status).toBe(404);
+    expect(getMedicationRequest).toHaveBeenCalledTimes(1);
+    expect(getMedicationRequest).toHaveBeenCalledWith("1");
+    expect(createPrescriptionOffer).toHaveBeenCalledTimes(0);
   });
 });
