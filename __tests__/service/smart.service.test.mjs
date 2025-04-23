17f140f07015a3bf69355c56578162b986c8f051
Magnus Gjerlaugsen <magnus.gjerlaugsen@oicloud.com>
2025-04-23 16:11:04 +0200

    Add tests for getCheckSmartSessionMiddleware in smart service

--- __tests__/service/smart.service.test.mjs
+++ __tests__/service/smart.service.test.mjs
@@ -1,12 +1,24 @@
+import { LogLevel } from "@credo-ts/core";
+
 import {
   getMedicationRequest,
   getAllMedicationRequests,
   setFhirClient,
+  getCheckSmartSessionMiddleware,
 } from "#src/service/smart.service.mjs";
+import { MyLogger } from "#src/util/logger.mjs";
+import { SMART_ROUTER_PATH } from "#src/controller/smart.controller.mjs";
 
 describe("smart service tests", () => {
   const fhirClientMock = {
     request: vi.fn(),
+    getState: vi.fn(),
+  };
+
+  const simpleAgentMock = {
+    config: {
+      logger: new MyLogger(LogLevel.off),
+    },
   };
 
   afterEach(() => {
@@ -43,6 +55,52 @@ describe("smart service tests", () => {
 
     it("should throw when fhirClient is undefined", async () => {
       await expect(getAllMedicationRequests()).rejects.toThrow();
+    });
+  });
+
+  describe("getCheckSmartSessionMiddleware", () => {
+    const requestObjectMock = vi.fn();
+    const responseObjectMock = vi.fn();
+    responseObjectMock.redirect = vi.fn();
+    const nextFunctionMock = vi.fn();
+
+    it("should return middleware that redirects to root page when fhirClient is undefined", () => {
+      const middleware = getCheckSmartSessionMiddleware(simpleAgentMock);
+
+      middleware(requestObjectMock, responseObjectMock, nextFunctionMock);
+
+      expect(responseObjectMock.redirect).toHaveBeenCalledTimes(1);
+      expect(responseObjectMock.redirect).toHaveBeenCalledWith("/");
+    });
+
+    it("should return middleware that redirects to session expired page when smart session has expired", () => {
+      fhirClientMock.getState.mockReturnValue({
+        expiresAt: 0,
+      });
+      setFhirClient(fhirClientMock);
+
+      const middleware = getCheckSmartSessionMiddleware(simpleAgentMock);
+
+      middleware(requestObjectMock, responseObjectMock, nextFunctionMock);
+
+      expect(responseObjectMock.redirect).toHaveBeenCalledTimes(1);
+      expect(responseObjectMock.redirect).toHaveBeenCalledWith(
+        `${SMART_ROUTER_PATH}/session_expired`,
+      );
+    });
+
+    it("should return middleware that calls next function when smart session has not expired", () => {
+      fhirClientMock.getState.mockReturnValue({
+        expiresAt: Number.MAX_VALUE,
+      });
+      setFhirClient(fhirClientMock);
+
+      const middleware = getCheckSmartSessionMiddleware(simpleAgentMock);
+
+      middleware(requestObjectMock, responseObjectMock, nextFunctionMock);
+
+      expect(responseObjectMock.redirect).toHaveBeenCalledTimes(0);
+      expect(nextFunctionMock).toHaveBeenCalledTimes(1);
     });
   });
 });
