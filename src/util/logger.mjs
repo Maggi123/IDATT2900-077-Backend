c7d6f8f92f0508ab65cd7c4fdc21fbb3c2c0e6c1
Magnus Gjerlaugsen <maggj@stud.ntnu.no>
2025-04-28 11:54:36 +0200

    Add licensing information for the replaceError function in the MyLogger class

--- src/util/logger.mjs
+++ src/util/logger.mjs
@@ -65,6 +65,22 @@ export class MyLogger extends BaseLogger {
     );
   }
 
+  /*
+   Copyright 2020-present Hyperledger Contributors.
+   Copyright 2021 Queen’s Printer for Ontario. Mostafa Youssef (https://github.com/MosCD3), Amit Padmani (https://github.com/nbAmit), Prasad Katkar (https://github.com/NB-PrasadKatkar), Mike Richardson (https://github.com/NB-MikeRichardson)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
   replaceError(_, value) {
     if (value instanceof Error) {
       return Object.getOwnPropertyNames(value).reduce(
